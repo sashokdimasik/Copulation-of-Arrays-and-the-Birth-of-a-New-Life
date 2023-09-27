9b22c8f586c6e67b9e84bd55d3f80265bb165029
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-27 23:23:09 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
  * Variant:      14                                               *
  * Date:         20.09.2023                                       *
  * Project name: Copulation of Arrays and the Birth of a New Life *
+ * OnlineGDB: https://onlinegdb.com/ZHeYG9xEnA                    *
  ******************************************************************/
 
 #include <iostream>
+
+using namespace std;
 
 double positiveSum(double *arr, int size) {
   double sum = 0;
@@ -20,29 +23,29 @@ int main()
 {
   int size;
   
-  std::cout << "Enter arrays' size: ";
-  std::cin >> size;
+  cout << "Enter arrays' size: ";
+  cin >> size;
   
   double *X = new double[size];
   double *Y = new double[size];
   double *Z = new double[size];
 
-  std::cout << "Enter elements of X array (mommy): ";
-  for (int i = 0; i < size; i++) std::cin >> X[i];
+  cout << "Enter elements of X array (mommy): ";
+  for (int i = 0; i < size; i++) cin >> X[i];
 
-  std::cout << "Enter elements of Y array (daddy): ";
-  for (int i = 0; i < size; i++) std::cin >> Y[i];
+  cout << "Enter elements of Y array (daddy): ";
+  for (int i = 0; i < size; i++) cin >> Y[i];
 
-  std::cout << "We got a Z! (baby): ";
+  cout << "We got a Z! (baby): ";
 
   for (int i = 0; i < size; i++) {
     Z[i] = X[i] * Y[i];
-    std::cout << Z[i] << ' ';
+    cout << Z[i] << ' ';
   }
 
-  std::cout << "\nSum of the positive numbers of X = " << positiveSum(X, size) << '\n';
-  std::cout << "Sum of the positive numbers of Y = " << positiveSum(Y, size) << '\n';
-  std::cout << "Sum of the positive numbers of Z = " << positiveSum(Z, size) << '\n';
+  cout << "\nSum of the positive numbers of X = " << positiveSum(X, size) << '\n';
+  cout << "Sum of the positive numbers of Y = " << positiveSum(Y, size) << '\n';
+  cout << "Sum of the positive numbers of Z = " << positiveSum(Z, size) << '\n';
 
   return 0;
 }
