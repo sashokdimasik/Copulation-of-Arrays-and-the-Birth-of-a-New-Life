fb4d89b3acf0bd91091b3b447f18c6087b122194
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-10-04 17:38:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * Variant:      14                                               *
  * Date:         20.09.2023                                       *
  * Project name: Copulation of Arrays and the Birth of a New Life *
- * OnlineGDB: https://onlinegdb.com/5m-cvvrQ5                     *
+ * OnlineGDB: https://onlinegdb.com/pWfgHff9gL                    *
  ******************************************************************/
 
 #include <iostream>
@@ -13,7 +13,7 @@ using namespace std;
 
 double positiveSum(double *arr, int size) {
   double sum = 0;
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; ++i) {
     if (arr[i] > 0) sum += arr[i];
   }
   return sum;
@@ -30,21 +30,25 @@ int main() {
   double *Z = new double[size];
 
   cout << "Enter elements of X array (mommy): ";
-  for (int i = 0; i < size; i++) cin >> X[i];
+  for (int i = 0; i < size; ++i) {
+    cin >> X[i];
+  }
 
   cout << "Enter elements of Y array (daddy): ";
-  for (int i = 0; i < size; i++) cin >> Y[i];
+  for (int i = 0; i < size; ++i) {
+    cin >> Y[i];
+  }
 
   cout << "We got a Z! (baby): ";
 
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; ++i) {
     Z[i] = X[i] * Y[i];
     cout << Z[i] << ' ';
   }
 
-  cout << "\nSum of the positive numbers of X = " << positiveSum(X, size) << '\n';
-  cout << "Sum of the positive numbers of Y = " << positiveSum(Y, size) << '\n';
-  cout << "Sum of the positive numbers of Z = " << positiveSum(Z, size) << '\n';
+  cout << "\nSum of the positive numbers of X = " << positiveSum(X, size) << '\n'
+       << "Sum of the positive numbers of Y = " << positiveSum(Y, size) << '\n'
+       << "Sum of the positive numbers of Z = " << positiveSum(Z, size);
 
   return 0;
 }
